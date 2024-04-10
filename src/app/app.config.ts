f08bf9bbd73b7ccd9c1784e5b6a73c2031da2ed8
Simon Rabuogi <symokevo@gmail.com>
2024-04-10 19:52:07 +0300

    Firebase connection
    - Create a new project in Firebase
    - Add a new web app to the project
    - Copy the config object from the Firebase SDK snippet
    - Install firebase via npm
    - Install @angular/fire via npm (to avoid the error: "Cannot find module '@angular/fire'")
    - Import all the necessary modules in the app.config.ts file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+  importProvidersFrom(
+    AngularFireModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
+    )]
 };
---
+++ src/app/core/constants/constant.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDYBUbipcu_It5gCGx9Tw3TOg9n9AYiDN8",
+  authDomain: "budget-app-b9fdc.firebaseapp.com",
+  databaseURL: "https://budget-app-b9fdc-default-rtdb.firebaseio.com",
+  projectId: "budget-app-b9fdc",
+  storageBucket: "budget-app-b9fdc.appspot.com",
+  messagingSenderId: "959406785325",
+  appId: "1:959406785325:web:d46d173712cfaa91d949a4",
+  measurementId: "G-JQ8BMJ5NZ5"
+};
