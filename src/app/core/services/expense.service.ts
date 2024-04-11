6ff6d07bf482b9e70fb0af934ee651256027641f
Simon Rabuogi <symokevo@gmail.com>
2024-04-11 04:52:06 +0300

    Create the crud methods in expenses service

--- src/app/core/services/expense.service.ts
+++ src/app/core/services/expense.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
+import { IExpense } from '../models/common.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpenseService {
+  private dbPath ='/expenses';
+  expensesRef: AngularFireList<any>;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) {
+    this.expensesRef = db.list(this.dbPath);
+   }
+
+    getAllExpenses() {
+      return this.expensesRef;
+    }
+
+    getExpense(key: string) {
+      return this.db.object(`${this.dbPath}/${key}`);
+    }
+
+    addExpense(expense: IExpense) {
+      this.expensesRef.push(expense);
+    }
+
+    updateExpense(key: string, expense: IExpense) {
+      this.expensesRef.update(key, expense);
+    }
+
+    deleteExpense(key: string) {
+      return this.expensesRef.remove(key);
+    }
 }
